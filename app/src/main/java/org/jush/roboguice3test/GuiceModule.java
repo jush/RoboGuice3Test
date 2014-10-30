0c2cf5397a2fc1e24d1f705be16a0e6ed51cba98
Ramon <oni.pool@gmail.com>
2014-10-31 00:00:57 +0200

    Added module constructor and re-ordered dependencies

--- app/src/main/java/org/jush/roboguice3test/GuiceModule.java
+++ app/src/main/java/org/jush/roboguice3test/GuiceModule.java
@@ -1,8 +1,16 @@
 package org.jush.roboguice3test;
 
+import android.app.Application;
+
 import com.google.inject.AbstractModule;
 
 public class GuiceModule extends AbstractModule {
+    private Application application;
+
+    public GuiceModule(Application application) {
+        this.application = application;
+    }
+
     @Override
     protected void configure() {
     }
