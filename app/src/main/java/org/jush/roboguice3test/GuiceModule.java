0726a14e447b81b5fd003fde0f0d03957a96e9e8
Ramon <oni.pool@gmail.com>
2014-10-30 23:42:10 +0200

    Added empty module as per http://goo.gl/4b8K6d

---
+++ app/src/main/java/org/jush/roboguice3test/GuiceModule.java
@@ -0,0 +1,9 @@
+package org.jush.roboguice3test;
+
+import com.google.inject.AbstractModule;
+
+public class GuiceModule extends AbstractModule {
+    @Override
+    protected void configure() {
+    }
+}
